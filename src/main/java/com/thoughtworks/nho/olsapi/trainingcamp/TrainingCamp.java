80e6838752fa8ec9d10e38eb63f398999886c56c
Xin Zhang <xin.zhang@rea-group.com>
2019-05-25 19:35:07 +0800

    create new camp

--- src/main/java/com/thoughtworks/nho/olsapi/trainingcamp/TrainingCamp.java
+++ src/main/java/com/thoughtworks/nho/olsapi/trainingcamp/TrainingCamp.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.nho.olsapi.trainingcamp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.Instant;
+import java.util.Date;
 
 @Entity
 public class TrainingCamp {
@@ -14,8 +17,10 @@ public class TrainingCamp {
     private int id;
     private String title;
     private String description;
-    private Instant startTime;
-    private Instant createdTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    private Date startTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    private Date createdTime;
 
     public int getId() {
         return id;
@@ -41,19 +46,19 @@ public class TrainingCamp {
         this.description = description;
     }
 
-    public Instant getStartTime() {
+    public Date getStartTime() {
         return startTime;
     }
 
-    public void setStartTime(Instant startTime) {
+    public void setStartTime(Date startTime) {
         this.startTime = startTime;
     }
 
-    public Instant getCreatedTime() {
+    public Date getCreatedTime() {
         return createdTime;
     }
 
-    public void setCreatedTime(Instant createdTime) {
+    public void setCreatedTime(Date createdTime) {
         this.createdTime = createdTime;
     }
 }
--- src/main/java/com/thoughtworks/nho/olsapi/trainingcamp/TrainingCampController.java
+++ src/main/java/com/thoughtworks/nho/olsapi/trainingcamp/TrainingCampController.java
@@ -1,14 +1,27 @@
 package com.thoughtworks.nho.olsapi.trainingcamp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class TrainingCampController {
 
-    TrainingCampController(TrainingCampService trainingCampService) {
+    public TrainingCampController(@Autowired TrainingCampService trainingCampService) {
         this.trainingCampService = trainingCampService;
     }
 
     private final TrainingCampService trainingCampService;
+
+    @PostMapping("trainingcamps")
+    public ResponseEntity<TrainingCamp> createOne(@RequestBody TrainingCamp newCamp) {
+        var createdCamp = trainingCampService.createOne(newCamp);
+        return ResponseEntity.created(URI.create("/trainingcamps/" + createdCamp.getId()))
+                .body(createdCamp);
+    }
 
 }
